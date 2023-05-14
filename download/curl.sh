a12855bae7b68632d518b62e503d78071487d4e3
TwinFlames <1787794+TwinFlames@users.noreply.github.com>
2023-05-13 23:28:06 -0400

    Create curl.sh

---
+++ download/curl.sh
@@ -0,0 +1,5 @@
+iso_url="https://software.download.prss.microsoft.com/dbazure/Win10_22H2_English_x64.iso?t=8f63a147-eb84-4773-9f7a-98e067e947b8&e=1684118885&h=1e5ead8e91bfe636cea0818865c5b098e3dd5e387da65b748c238999ef895fcf"
+
+curl --location --remote-name -C - $iso_url
+
+#prime: https://base64.co.za/how-to-download-windows-10-iso-with-curl/ , https://www.google.com/search?q=curl+continue+partial+download
